2967069c81d57995217d98903a536e223eafda0a
lieandie <kirilllik@gmail.com>
2018-04-21 04:21:41 +0300

    input + canvas preview

--- src/app/modules/ngx-image-picker/ngx-image-picker.component.ts
+++ src/app/modules/ngx-image-picker/ngx-image-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-image-picker',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgxImagePickerComponent implements OnInit {
 
+  @ViewChild('imageInput') imageInput;
+  @ViewChild('canvas') canvas;
+
+  private image;
+
   constructor() { }
 
   ngOnInit() {
+    this.image = new Image();
+    this.image.onload = e => this.drawCanvas();
+  }
+
+  private upload() {
+    const inputElement = this.imageInput.nativeElement;
+    if (inputElement.files && inputElement.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => this.image.src = e.target.result;
+      reader.readAsDataURL(inputElement.files[0]);
+    }
+  }
+
+  private drawCanvas() {
+    const canvas = this.canvas.nativeElement;
+    const ctx = canvas.getContext('2d');
+    canvas.width = this.image.width;
+    canvas.height = this.image.height;
+    ctx.drawImage(this.image, 0, 0);
   }
 
 }
