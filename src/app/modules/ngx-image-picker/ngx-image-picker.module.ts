435a983c8a01438ac72e9244a0eb5debad1489a3
lieandie <kirilllik@gmail.com>
2018-04-23 22:32:26 +0300

    back to single main component (no customization but saves integrity and impoves usage experience)

--- src/app/modules/ngx-image-picker/ngx-image-picker.module.ts
+++ src/app/modules/ngx-image-picker/ngx-image-picker.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxImagePickerComponent } from './img-picker/ngx-image-picker.component';
 import { ImgPreviewCardComponent } from './img-preview-card/img-preview-card.component';
-import { PureCardComponent } from './pure-card/pure-card.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NgxImagePickerComponent, ImgPreviewCardComponent, PureCardComponent],
+  declarations: [NgxImagePickerComponent, ImgPreviewCardComponent],
   exports: [
     NgxImagePickerComponent,
     ImgPreviewCardComponent
