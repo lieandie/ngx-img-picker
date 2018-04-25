570eaf44c1c1b9e81cfe890f86e7bddbfa1133dc
lieandie <kirilllik@gmail.com>
2018-04-26 00:18:40 +0300

    shadows style reworked

--- src/app/modules/ngx-md-button/ngx-md-round-button/ngx-md-round-button.component.ts
+++ src/app/modules/ngx-md-button/ngx-md-round-button/ngx-md-round-button.component.ts
@@ -3,7 +3,7 @@ import {Component, Input, OnInit, ViewChild} from '@angular/core';
 @Component({
   selector: 'app-ngx-md-round-button',
   templateUrl: './ngx-md-round-button.component.html',
-  styleUrls: ['./ngx-md-round-button.component.css']
+  styleUrls: ['./ngx-md-round-button.component.css', '../button.css']
 })
 export class NgxMdRoundButtonComponent implements OnInit {
 
